61a0354a19949b7246b8cc63dfd1d3f0b4e8a7f2
min33sky <min33sky@naver.com>
2022-09-09 22:37:52 +0900

    feat: create api

--- src/page/Create.tsx
+++ src/page/Create.tsx
@@ -1,5 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import supabase from '../config/supabaseConfig';
 
 export default function Create() {
-  return <div className="page create">Create</div>;
+  const navigate = useNavigate();
+  const [title, setTitle] = useState('');
+  const [method, setMethod] = useState('');
+  const [rating, setRating] = useState(0);
+  const [formError, setFormError] = useState<string | null>(null);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!title || !method || !rating) {
+      setFormError('Please fill in all the fields correctly.');
+      return;
+    }
+
+    const { data, error } = await supabase
+      .from('smoothies')
+      .insert([{ title, method, rating }]);
+
+    if (error) {
+      console.log(error);
+      setFormError('Please fill in all the fields correctly.');
+    }
+    if (data) {
+      console.log(data);
+      setFormError(null);
+      navigate('/');
+    }
+  };
+
+  return (
+    <div className="page create">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label htmlFor="method">Method:</label>
+        <textarea
+          id="method"
+          value={method}
+          onChange={(e) => setMethod(e.target.value)}
+        />
+
+        <label htmlFor="rating">Rating:</label>
+        <input
+          type="number"
+          id="rating"
+          value={rating}
+          onChange={(e) => setRating(parseInt(e.target.value, 10))}
+        />
+
+        <button>Create Smoothie Recipe</button>
+
+        {formError && <p className="error">{formError}</p>}
+      </form>
+    </div>
+  );
 }
