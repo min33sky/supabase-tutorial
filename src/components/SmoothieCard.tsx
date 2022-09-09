4a7ce5fa3c5162188b1405172eb386ddd915cf5a
min33sky <min33sky@naver.com>
2022-09-09 23:08:52 +0900

    feat: fetch single data

--- src/components/SmoothieCard.tsx
+++ src/components/SmoothieCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { Smoothie } from '../page/Home';
 
 interface Props {
@@ -10,6 +11,11 @@ export default function SmoothieCard({ smoothie }: Props) {
       <h3>{smoothie.title}</h3>
       <p>{smoothie.method}</p>
       <div className="rating">{smoothie.rating}</div>
+      <div className="buttons">
+        <Link to={`/${smoothie.id}`}>
+          <i className="material-icons">edit</i>
+        </Link>
+      </div>
     </div>
   );
 }
--- src/page/Update.tsx
+++ src/page/Update.tsx
@@ -1,5 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import supabase from '../config/supabaseConfig';
+import { Smoothie } from './Home';
 
 export default function Update() {
-  return <div className="page update">Update</div>;
+  const navigate = useNavigate();
+
+  const { id } = useParams<{ id: string }>();
+  const [title, setTitle] = useState('');
+  const [method, setMethod] = useState('');
+  const [rating, setRating] = useState(0);
+
+  useEffect(() => {
+    const fetchSmoothie = async () => {
+      const { data, error } = await supabase
+        .from<Smoothie>('smoothies')
+        .select()
+        .eq('id', id!.toString())
+        .single();
+
+      if (error) {
+        navigate('/', { replace: true });
+      }
+      if (data) {
+        setTitle(data.title);
+        setMethod(data.method);
+        setRating(data.rating);
+      }
+    };
+
+    fetchSmoothie();
+  }, [id, navigate]);
+
+  return (
+    <div className="page create">
+      <form>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label htmlFor="method">Method:</label>
+        <textarea
+          id="method"
+          value={method}
+          onChange={(e) => setMethod(e.target.value)}
+        />
+
+        <label htmlFor="rating">Rating:</label>
+        <input
+          type="number"
+          id="rating"
+          value={rating}
+          onChange={(e) => setRating(parseInt(e.target.value, 10))}
+        />
+
+        <button>Update Smoothie Recipe</button>
+      </form>
+    </div>
+  );
 }
